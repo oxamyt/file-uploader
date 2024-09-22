81f046afb454b897fd5446d8ef365866ac8b8176
oxamyt <dentez.danil@gmail.com>
2024-09-22 12:38:10 +0300

    setup tailwind-css and style userAuthentication pages

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{ejs,pug,html}", "./public/**/*.{js,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
